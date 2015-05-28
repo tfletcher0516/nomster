4a1d024ceedc2ce8cb0b1212b2b005c1753f9eed
Tim Fletcher <fletcher0516@gmail.com>
2015-05-28 18:03:56 +0000

    Added Devise gem and settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,5 @@
 Nomster::Application.configure do
+  config.action_mailer.default_url_options = { :host => 'https://nomster-tim-fletcher.herokuapp.com/'}
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
