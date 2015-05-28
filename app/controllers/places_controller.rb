9b1a35284363e3a167548502108850b87b4af188
Tim Fletcher <fletcher0516@gmail.com>
2015-05-28 01:49:20 +0000

    Added Pagination and New forms

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,5 +1,11 @@
 class PlacesController < ApplicationController
 	def index
-		@places = Place.all
+
+		@places = Place.all.paginate(page: params[:page], per_page: 5)
+
+	end
+
+	def new
+		@place = Place.new
 	end
 end
