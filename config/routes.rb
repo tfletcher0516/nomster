fb212633c626e3a4c6b6a2deeddf9ade694cc39e
Tim Fletcher <fletcher0516@gmail.com>
2015-06-05 01:56:11 +0000

    Added submission button that works

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Nomster::Application.routes.draw do
   devise_for :users
   root 'places#index'
-
-  resources :places
+  #resources :comments, :only => :create
+  resources :places do
+   resources :comments, :only => :create
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
