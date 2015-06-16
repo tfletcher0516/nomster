9122c12b551f243e6b493f9b487d23ada071a446
Tim Fletcher <fletcher0516@gmail.com>
2015-06-16 23:13:57 +0000

    Enabled Comment email and turned off testing email

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,7 @@
 class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :place
+  after_create :send_comment_email
 
   validates :message, :presence => true, :length => {:minimum => 4, :maximum => 300 }
   RATINGS = {
@@ -10,8 +11,12 @@ class Comment < ActiveRecord::Base
     'four stars' => '4_stars',
     'five stars' => '5_stars'
   }
-
+  
   def humanized_rating
     RATINGS.invert[self.rating]
+  end
+
+   def send_comment_email
+      NotificationMailer.comment_added(self).deliver
   end
 end
