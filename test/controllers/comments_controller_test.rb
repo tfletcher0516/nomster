3bfdef4075d259bda8577bda410c707b6b53b814
Tim Fletcher <fletcher0516@gmail.com>
2015-06-25 02:11:55 +0000

    Added multiple tests

--- test/controllers/comments_controller_test.rb
+++ test/controllers/comments_controller_test.rb
@@ -1,7 +1,21 @@
 require 'test_helper'
 
 class CommentsControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+ test "Create Comment" do
+  user = FactoryGirl.create(:user)
+  sign_in user
+
+  place = FactoryGirl.create(:place, :user => user)
+  assert_difference 'place.comments.count' do
+  	post :create, :place_id => place.id, :comment => {
+
+  		:rating => "2_stars",
+  		:message => "Meh whatever"
+  	}
+  	end
+
+  	assert_redirected_to place_path(place)
+  	comment = Comment.last
+  	puts comment.message
+  end
 end
--- test/models/comment_test.rb
+++ test/models/comment_test.rb
@@ -3,10 +3,11 @@ require 'factory_girl_rails'
 
 class CommentTest < ActiveSupport::TestCase
  test "humanized rating" do
-  user = FactoryGirl.create(:user)
-  place = FactoryGirl.create(:place)
+  #user = FactoryGirl.create(:user, :email => "ken@thefirehoseproject.com")
+  #place = FactoryGirl.create(:place, :user => user)
+	#comment = FactoryGirl.create(:comment, :place => place)
 	comment = FactoryGirl.create(:comment)
-  	
+  	comment.place.user.email
   	expected = "two stars"
   	actual = comment.humanized_rating
   	assert_equal expected,actual
